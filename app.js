5990a52725892572d81ff30dfdf529cbd7ee7e63
Felix Stumvoll <felix.stumvoll@gmail.com>
2019-01-14 10:12:55 +0100

    removed db connection

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ var serverPort = process.env.PORT || port;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-mongoose
-    .connect(
-        dbUrl,
-        { useNewUrlParser: true }
-    )
-    .catch(error => {
-        console.log(`Error connecting to Database: ${error}`);
-        return;
-    })
-    .then(() => {
-        console.log('connected to Database');
-        app.use(cors());
-        app.use('/api', router);
-        app.use(exceptionMiddleware);
-
-        app.listen(serverPort, () => {
-            console.log(`Server Running on Port:${serverPort}`);
-        });
-    });
+// mongoose
+//     .connect(
+//         dbUrl,
+//         { useNewUrlParser: true }
+//     )
+//     .catch(error => {
+//         console.log(`Error connecting to Database: ${error}`);
+//         return;
+//     })
+//     .then(() => {
+//         console.log('connected to Database');
+//
+//     });
+app.use(cors());
+app.use('/api', router);
+app.use(exceptionMiddleware);
+app.listen(serverPort, () => {
+    console.log(`Server Running on Port:${serverPort}`);
+});
